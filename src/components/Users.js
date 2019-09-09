31b482a18587146c698c330533b2284b324c6731
Elyse Burmester <burmesterelyse@gmail.com>
2019-09-09 20:21:49 +0000

    Automatically backed up by Learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -13,6 +13,11 @@ class Users extends Component {
   }
 }
 
-//add mapStateToProps here
+const mapStateToProps = (state) => {
+  return {
+    users: state.users,
+    numberOfUsers: state.users.length
+  };
+};
 
-export default Users
+export default connect(mapStateToProps)(Users)
